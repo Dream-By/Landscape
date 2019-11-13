4794b0272b13afa4a4b489bfeab64a472839f862
dzmitry.dzianisau@gmail.com <Dream-By>
2019-11-13 09:11:27 +0300

    Добавили изменения в код.

--- app/src/main/java/com/example/landscapeorientation/MainActivity.java
+++ app/src/main/java/com/example/landscapeorientation/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.landscapeorientation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        }
+
+    private String getScreenOrientation(){
+        if (getResources().getConfiguration().orientation== Configuration.ORIENTATION_PORTRAIT)
+            return "Портретная ориентация";
+        else if (getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE)
+            return "Альбомная ориентация";
+        else
+            return"";
+    }
+
+    public void OnClick(View view) {
+        TextView editText = findViewById(R.id.textView);
+        editText.setText(getScreenOrientation());
+
     }
 }
