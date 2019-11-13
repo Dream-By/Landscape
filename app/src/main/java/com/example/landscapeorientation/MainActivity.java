53893312fcc9c4b15dec66016457f861f2b372be
dzmitry.dzianisau@gmail.com <Dream-By>
2019-11-13 16:56:46 +0300

    Пересоздание активности

--- app/src/main/java/com/example/landscapeorientation/MainActivity.java
+++ app/src/main/java/com/example/landscapeorientation/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.landscapeorientation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Surface;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,13 +16,24 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mButton;
+    static final String ORIENTATION_PORTRAIT = "Портретный режим";
+    static final String ORIENTATION_LANDSCAPE = "Альбомный режим";
+    //определяем изменение ориентации экрана
+    boolean mState = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button)findViewById(R.id.button3);
+
+        //текст по умолчанию
+        mButton.setText(ORIENTATION_LANDSCAPE);
         }
 
-    private String getScreenOrientation(){
+        private String getScreenOrientation(){
         if (getResources().getConfiguration().orientation== Configuration.ORIENTATION_PORTRAIT)
             return "Портретная ориентация";
         else if (getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE)
@@ -27,9 +42,48 @@ public class MainActivity extends AppCompatActivity {
             return"";
     }
 
+    private String getRotateOrientation(){
+        int rotate = getWindowManager().getDefaultDisplay().getRotation();
+        switch (rotate){
+            case Surface.ROTATION_0:
+                return "Не поворачивали";
+                case Surface.ROTATION_90:
+                    return "Повернули на 90 градусов по часовой стрелке";
+                    case Surface.ROTATION_180:
+                        return "Повернули на 180 градусов";
+                        case Surface.ROTATION_270:
+                            return "Повернули на 270 градусов";
+                            default:
+                                return "Непонятно...";
+        }
+    }
+
     public void OnClick(View view) {
         TextView editText = findViewById(R.id.textView);
         editText.setText(getScreenOrientation());
+        }
+
+    public void OnRotation(View view) {
+        TextView editText = findViewById(R.id.textView);
+        editText.setText(getRotateOrientation());
+    }
+
+    public void OnLand(View view) {
+        // state FALSE: переключаемся на LANDSCAPE
+        if (!mState){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            TextView editText = findViewById(R.id.textView);
+            editText.setText(ORIENTATION_LANDSCAPE);
+        }
+        //state TRUE: переключаемся на PORTRAIT
+        else
+        {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            TextView editText = findViewById(R.id.textView);
+            editText.setText(ORIENTATION_PORTRAIT);
+        }
+        //обновляем state на противоположное значение
+        mState = !mState;
 
     }
 }
